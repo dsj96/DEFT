438d75db4c5022ff69cd865d9fb1fc8c189840e5
dsj96 <51779172+dsj96@users.noreply.github.com>
2024-08-20 19:02:54 +0800

    Create get_knowledge.sh

---
+++ examples/get_knowledge.sh
@@ -0,0 +1,6 @@
+export CUDA_VISIABLE_DEVICES='0'
+
+deepspeed --include=localhost:${CUDA_VISIABLE_DEVICES} --master_port 29700  detect_ds3.py \
+	--model_path '../pre_trained_model/llama2-13b-hf' \
+	--start_index 0 \
+	--end_index 320000
